c29736d9f6252d55cfa2a9bd1dc8f4e2749f1532
Pham Viet Toan <viettoan290696@gmail.com>
2017-09-12 13:22:45 +0700

    list user ui

---
+++ resources/lang/en/message.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'new_user' => 'New User',
+    'users' => 'Users',
+    'id' => 'ID',
+    'name' => 'Name',
+    'phone' => 'Phone',
+    'email' => 'Email',
+    'action' => 'Action',
+    'dashboard' => 'DASHBOARD',
+    'component' => 'COMPONENT',
+    'manage_user' => 'Manage User',
+    'medicine' => 'Medicine',
+    'logout' => 'Logout',
+];
