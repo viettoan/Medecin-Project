2e533a9ff23098fda2afed5a6d74f1576d91da54
Pham Viet Toan <viettoan290696@gmail.com>
2017-10-21 10:19:22 +0700

    fix new post index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,43 @@
   @include('sites._include.main-slide')
   @include('sites._include.utility')
   @include('sites._include.video-contact')
-  @include('sites._include.news')
+  <section class="news">
+    <div class="container">
+      <div class="row">
+      <br>
+        <div class="col-sm-12">
+            <h2 class='topic-name'>Tin tức mới nhất</h2>
+            <div class="line"></div>
+         </div>
+      </div>
+      <div class="row">
+          <div class="panel panel-default">
+              @if (isset($posts))
+                @foreach ($posts as $post)
+                    <div class="row-item row">
+                        <div class="col-md-4 col-lg-3">
+                            <a href="detail.html">
+                                <img  class="img-responsive img-post img-thumbnail" src="{{ $post->image }}" alt="">
+                            </a>
+                        </div>
+                        <div class="col-md-8 col-lg-9">
+                            <h5>{{ $post->title }}</h5>
+                            <small><i class='fa fa-calendar-o'></i><i>{{ $post->created_at }}</i></small>
+                            <p>{{ substr($post->content, 0, 400) }}...</p>
+                            <a class="btn btn-outline-success btn-sm" href="{{ route('page.post.show', ['category' => str_replace(' ', '-', $post->categories->name), 'post_name' => str_replace(' ', '-', $post->title)] ) }}">Xem thêm <span class="glyphicon glyphicon-chevron-right"></span></a>
+                        </div>
+                        <div class="break"></div>
+
+                    </div>
+                    <hr>
+                  @endforeach
+                @endif
+              <!-- /.row -->
+          </div>
+      </div>
+
+    </div>
+  </section>
   @include('sites._include.footer')
 @endsection
 @section('style')
@@ -14,7 +50,6 @@
  {{ Html::style('css/sites/_include/mainslide.css') }}
  {{ Html::style('css/sites/_include/tienich.css') }}
  {{ Html::style('css/sites/_include/video-contact.css') }}
- {{ Html::style('css/sites/_include/news.css') }}
  {{ Html::style('css/sites/_include/footer.css') }}
 @endsection
 
