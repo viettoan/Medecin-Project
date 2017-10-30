7d2e1e4c6b8d717c81033b4bac0a629aa69927e6
tranvanmy <anhmynd19101995@gmail.com>
2017-10-30 15:11:33 +0700

    fix post

--- resources/views/sites/post/index.blade.php
+++ resources/views/sites/post/index.blade.php
@@ -50,12 +50,12 @@
                           <!-- item -->
                           <div class="row" style="margin-top: 10px;">
                               <div class="col-md-5">
-                                  <a href="#">
+                                  <a href="{{ route('page.post.show', ['category' => str_replace(' ', '-', $item->categories->name), 'post_name' => str_replace(' ', '-', $item->title)] ) }}">
                                       <img class="img-responsive" width="100%" height='100%' src="{{ $item->image }}" alt="">
                                   </a>
                               </div>
                               <div class="col-md-7 ">
-                                  <a class='title-related-post' href="{{ route('page.post.show', ['category' => str_replace(' ', '-', $post->categories->name), 'post_name' => str_replace(' ', '-', $post->title)] ) }}">{{ substr($item->title, 0, 20) }}...</a>
+                                  <a class='title-related-post' href="{{ route('page.post.show', ['category' => str_replace(' ', '-', $item->categories->name), 'post_name' => str_replace(' ', '-', $item->title)] ) }}">{{ substr($item->title, 0, 20) }}...</a>
                               </div>
                               <div class="break"></div>
                           </div>
